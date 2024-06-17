405785528fd821d8a0f25edf2c364e8694441fb1
lenafiedor <lenafiedor@o2.pl>
2024-06-17 18:00:35 +0200

    Add docstrings, type hinting & annotations

--- src/amino.py
+++ src/amino.py
@@ -1,9 +1,11 @@
 import pygame
 import random
 import os
+from main import MAIN_DIR
 
 
-images_dir = '../images/aminos/'
+# define the directory containing aminoacid images and the list of image files
+images_dir = os.path.join(MAIN_DIR, '../images/aminos/')
 amino_pics = [
     'ala.png', 'arg.png', 'asn.png', 'asp.png', 'cys.png', 'gln.png', 
     'glu.png', 'gly.png', 'his.png', 'ile.png', 'leu.png', 'lys.png', 
@@ -13,8 +15,21 @@ amino_pics = [
 
 class Aminoacid(pygame.sprite.Sprite):
 
+    '''
+    Represents aminoacid in the game.
+
+    Attributes:
+        image (pygame.Surface): The image representing the aminoacid.
+        rect (pygame.Rect): The position of the aminoacid on the game screen.
+    '''
+
     def __init__(self):
+
+        '''
+        Initializes a new Aminoacid instance by selecting a random image and position.
+        '''
+
         super().__init__()
         self.image = pygame.image.load(os.path.join(images_dir, random.choice(amino_pics)))
-        random_position = pygame.Rect(random.randint(0, 24) * 32, random.randint(1, 18) * 18, 32, 32)
+        random_position = pygame.Rect(random.randint(0, 24)*32, random.randint(1, 18)*18, 32, 32)
         self.rect = random_position
